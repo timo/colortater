59e20eab01f6cbb001c1d229e58777f42707bcd0
Timo Paulssen <timonator@perpetuum-immobile.de>
2012-05-11 04:21:02 +0200

    split ColorRotator/*Window, fix --headless.

--- colortate.py
+++ colortate.py
@@ -25,10 +25,8 @@ def colored_icon(color_a, color_b):
     qp.end()
     return icon
 
-class ColorRotatorWindow(QDialog):
+class ColorRotator(object):
     def __init__(self, stylefiles):
-        super(ColorRotatorWindow, self).__init__()
-
         self.stylefiles = {}
         self.knowncolors = {}
 
@@ -36,55 +34,12 @@ class ColorRotatorWindow(QDialog):
         self.group_rotations = []
         self.color_items = {}
 
-        self.sliders = []
-        self.setup_ui()
-
         for stylefile in stylefiles:
             self.open_stylefile(stylefile)
 
         self.adjustment_filename = path.join(path.dirname(sorted(self.stylefiles.keys())[0]), "colortate.dat")
 
         self.read_adjustment_values()
-
-    def setup_ui(self):
-        self.cotree = QTreeWidget()
-        self.cotree.setIconSize(QSize(ICON_W, ICON_H))
-        self.cotree.setHeaderHidden(True)
-        self.cotree.currentItemChanged.connect(self.on_tree_item_changed)
-
-        self.write_button = QPushButton("save")
-        self.write_button.clicked.connect(self.write_files)
-
-        self.reset_button = QPushButton("reset all")
-
-        self.slider_box = QHBoxLayout()
-
-        self.vlayout = QVBoxLayout()
-        self.vlayout.addWidget(self.reset_button)
-        self.vlayout.addLayout(self.slider_box)
-        self.vlayout.addWidget(self.write_button)
-
-        self.hlayout = QHBoxLayout()
-        self.hlayout.addWidget(self.cotree)
-        self.hlayout.addLayout(self.vlayout)
-
-        self.setLayout(self.hlayout)
-
-    def on_tree_item_changed(self, current, prev):
-        for activate, blob in zip((False, True), (prev, current)):
-            if not blob: continue
-
-            if blob.childCount() == 0:
-                group = blob.parent()
-            else:
-                group = blob
-
-            gnum = int(group.text(0)) - 1
-            slider = self.sliders[gnum]
-            if activate:
-                slider.setStyleSheet("background-color: lightblue;")
-            else:
-                slider.setStyleSheet("")
 
     def new_color(self, color):
         for group in self.groups:
@@ -98,45 +53,6 @@ class ColorRotatorWindow(QDialog):
         # no group was found, create a new one.
         self.groups.append([color])
         self.group_rotations.append(0)
-
-        group_number = len(self.groups) - 1
-        def adjust_color(number):
-            self.group_rotations[group_number] = number
-            for col in self.groups[group_number]:
-                try:
-                    self.color_items[col.name()].setIcon(0, colored_icon(col, self.color_transform(col, group_number)))
-                    self.cotree.scrollToItem(self.color_items[self.groups[group_number][0].name()])
-                except KeyError:
-                    pass # we may have not created the tree at all yet.
-
-        hue = color.hue()
-
-        layout = QVBoxLayout()
-
-        reset = QPushButton("R")
-        reset.setFixedWidth(25)
-        layout.addWidget(reset)
-
-        label = QLabel(str(len(self.groups)))
-        layout.addWidget(label)
-
-        slider = QSlider()
-        self.sliders.append(slider)
-        slider.setRange(-hue, 360 - hue)
-        slider.setValue(0)
-        slider.setTracking(True)
-        slider.valueChanged.connect(adjust_color)
-
-        def reset_color():
-            slider.setSliderPosition(0)
-            adjust_color(0)
-
-        reset.clicked.connect(reset_color)
-        self.reset_button.clicked.connect(reset_color)
-
-        layout.addWidget(slider)
-
-        self.slider_box.addLayout(layout)
 
     def open_stylefile(self, filename):
         print "reading stylefile", filename
@@ -168,8 +84,6 @@ class ColorRotatorWindow(QDialog):
 
         self.stylefiles[filename] = new_text
 
-        self.update_color_tree()
-
     def read_adjustment_values(self):
         try:
             print "reading adjustment values from %s" % self.adjustment_filename
@@ -181,27 +95,8 @@ class ColorRotatorWindow(QDialog):
                 for gidx, group in enumerate(self.groups):
                     if group[0].name() in group_values:
                         self.group_rotations[gidx] = group_values[group[0].name()]
-                        self.sliders[gidx].setValue(group_values[group[0].name()])
         except IOError:
             print "no adjustment file found at %s" % (self.adjustment_filename)
-
-    def update_color_tree(self):
-        self.cotree.clear()
-        self.color_items = {}
-        for index, g in enumerate(self.groups):
-            par_it = QTreeWidgetItem(str(index + 1))
-            self.cotree.addTopLevelItem(par_it)
-            self.cotree.expandItem(par_it)
-
-            sorted_colors = g[:]
-            sorted_colors.sort(key = lambda c: c.value())
-
-            for color in sorted_colors:
-                col_it = QTreeWidgetItem([color.name()])
-                col_it.setIcon(0, colored_icon(color, self.color_transform(color)))
-                self.color_items[color.name()] = col_it
-
-                par_it.addChild(col_it)
 
     def color_transform(self, color, groupnum=None):
         if not groupnum:
@@ -235,6 +130,117 @@ class ColorRotatorWindow(QDialog):
                 pairs.append("%s: %s\n" % (repr_name, adjustment))
             f.writelines(pairs)
 
+class ColorRotatorWindow(QDialog):
+    def __init__(self, stylefiles):
+        super(ColorRotatorWindow, self).__init__()
+
+        self.cr = ColorRotator(stylefiles)
+
+        self.sliders = []
+        self.setup_ui()
+        self.update_color_tree()
+        self.setup_sliders()
+
+    def setup_ui(self):
+        self.cotree = QTreeWidget()
+        self.cotree.setIconSize(QSize(ICON_W, ICON_H))
+        self.cotree.setHeaderHidden(True)
+        self.cotree.currentItemChanged.connect(self.on_tree_item_changed)
+
+        self.write_button = QPushButton("save")
+        self.write_button.clicked.connect(self.cr.write_files)
+
+        self.reset_button = QPushButton("reset all")
+
+        self.slider_box = QHBoxLayout()
+
+        self.vlayout = QVBoxLayout()
+        self.vlayout.addWidget(self.reset_button)
+        self.vlayout.addLayout(self.slider_box)
+        self.vlayout.addWidget(self.write_button)
+
+        self.hlayout = QHBoxLayout()
+        self.hlayout.addWidget(self.cotree)
+        self.hlayout.addLayout(self.vlayout)
+
+        self.setLayout(self.hlayout)
+
+    def setup_sliders(self):
+        for group_number, group in enumerate(self.cr.groups):
+            self.make_slider(group_number, group)
+
+    def make_slider(self, group_number, group):
+        color = group[0]
+
+        def adjust_color(number):
+            self.cr.group_rotations[group_number] = number
+            for col in self.cr.groups[group_number]:
+                self.color_items[col.name()].setIcon(0, colored_icon(col, self.cr.color_transform(col, group_number)))
+                self.cotree.scrollToItem(self.color_items[self.cr.groups[group_number][0].name()])
+
+        hue = self.cr.color_transform(color, group_number).hue()
+
+        layout = QVBoxLayout()
+
+        reset = QPushButton("R")
+        reset.setFixedWidth(25)
+        layout.addWidget(reset)
+
+        label = QLabel(str(group_number + 1))
+        layout.addWidget(label)
+
+        slider = QSlider()
+        self.sliders.append(slider)
+        slider.setRange(-hue, 360 - hue)
+        slider.setValue(0)
+        slider.setTracking(True)
+        slider.valueChanged.connect(adjust_color)
+
+        def reset_color():
+            slider.setSliderPosition(0)
+            adjust_color(0)
+
+        reset.clicked.connect(reset_color)
+        self.reset_button.clicked.connect(reset_color)
+
+        layout.addWidget(slider)
+
+        self.slider_box.addLayout(layout)
+
+    def on_tree_item_changed(self, current, prev):
+        for activate, blob in zip((False, True), (prev, current)):
+            if not blob: continue
+
+            if blob.childCount() == 0:
+                group = blob.parent()
+            else:
+                group = blob
+
+            gnum = int(group.text(0)) - 1
+            slider = self.sliders[gnum]
+            if activate:
+                slider.setStyleSheet("background-color: lightblue;")
+            else:
+                slider.setStyleSheet("")
+
+    def update_color_tree(self):
+        self.cotree.clear()
+        self.color_items = {}
+        for index, g in enumerate(self.cr.groups):
+            par_it = QTreeWidgetItem(str(index + 1))
+            self.cotree.addTopLevelItem(par_it)
+            self.cotree.expandItem(par_it)
+
+            sorted_colors = g[:]
+            sorted_colors.sort(key = lambda c: c.value())
+
+            for color in sorted_colors:
+                col_it = QTreeWidgetItem([color.name()])
+                col_it.setIcon(0, colored_icon(color, self.cr.color_transform(color)))
+                self.color_items[color.name()] = col_it
+
+                par_it.addChild(col_it)
+
 if __name__ == "__main__":
     import sys
     import argparse
@@ -249,13 +255,13 @@ if __name__ == "__main__":
 
     args = ap.parse_args()
 
-    app = QApplication(sys.argv)
-    w = ColorRotatorWindow(args.filenames)
+    if args.headless:
+        app = QApplication(sys.argv, not args.headless)
+        cr = ColorRotator(args.filenames)
+        cr.write_files()
 
-    if not args.headless:
-        w.show()
     else:
-        w.write_files()
-        sys.exit(0)
-
-    sys.exit(app.exec_())
+        app = QApplication(sys.argv, not args.headless)
+        w = ColorRotatorWindow(args.filenames)
+        w.show()
+        sys.exit(app.exec_())
